ccea3f118c5741b0c596ba1c36cc52c80fdd6145
zgardner11 <zgardner.cinema@gmail.com>
2023-04-30 19:00:52 -0700

    Product updates

--- Scripts/extra.js
+++ Scripts/extra.js
@@ -6,3 +6,63 @@ const options = {
 
 
 
+
+// function activeNav() {
+//   const sections = document.querySelectorAll(".finishing-product")
+//   const navContainer = document.createElement("nav");
+//   const navItems = Array.from(sections).map(section => {
+//     return `
+//       <div class = "nav
+//     `;
+//   }); 
+
+
+
+  
+
+//   console.log(sections);
+//   console.log(navItems);
+// };
+
+// activeNav();
+
+// Accessing array.map method, which is not available to node list by itself as sections.map
+
+
+
+//  <nav id = "products-nav">
+// <ul>
+//     <li>
+//         <a href="#textures" class = "product-nav product-nav-active">Textures</a>
+//     </li>
+    
+//     <li>
+//         <a href="#patch" class = "product-nav">Patch & Repair</a>
+
+//     </li>
+
+//     <li>
+//         <a href="#joints" class = "product-nav">Joint Compounds</a>
+//     </li>
+
+//     <li>
+//         <a href="#manuhousing" class = "product-nav">Manufactured Housing</a>
+//     </li>
+
+//     <li>
+//         <a href="#dcs" class = "product-nav">Drywall Completion Systems</a>
+//     </li>
+
+//     <li>
+//         <a href="#stc" class = "product-nav">Setting Type Compounds</a>
+//     </li>
+
+//     <li>
+//         <a href="#concrete" class = "product-nav">Concrete Finishing</a>
+//     </li>
+
+//     <li>
+//         <a href="#wallboard" class = "product-nav">Wallboard Tape</a>
+//     </li>
+// </ul>
+// </nav> 
--- Scripts/main.js
+++ Scripts/main.js
@@ -8,6 +8,15 @@ burger.addEventListener('click', () => {
     navMenu.classList.toggle('active');
 })
 
+// Tech data drop down items
+
+const dataHeader = document.querySelector('.dataHeader')
+const dataItem = document.querySelector('.dataItem')
+
+dataHeader.addEventListener('click', () => {
+    dataItem.classList.toggle('active');
+}
+)
 
 
 
@@ -18,65 +27,3 @@ burger.addEventListener('click', () => {
 
 
 
-
-
-
-// function activeNav() {
-//   const sections = document.querySelectorAll(".finishing-product")
-//   const navContainer = document.createElement("nav");
-//   const navItems = Array.from(sections).map(section => {
-//     return `
-//       <div class = "nav
-//     `;
-//   }); 
-
-
-
-  
-
-//   console.log(sections);
-//   console.log(navItems);
-// };
-
-// activeNav();
-
-// Accessing array.map method, which is not available to node list by itself as sections.map
-
-
-
-//  <nav id = "products-nav">
-// <ul>
-//     <li>
-//         <a href="#textures" class = "product-nav product-nav-active">Textures</a>
-//     </li>
-    
-//     <li>
-//         <a href="#patch" class = "product-nav">Patch & Repair</a>
-
-//     </li>
-
-//     <li>
-//         <a href="#joints" class = "product-nav">Joint Compounds</a>
-//     </li>
-
-//     <li>
-//         <a href="#manuhousing" class = "product-nav">Manufactured Housing</a>
-//     </li>
-
-//     <li>
-//         <a href="#dcs" class = "product-nav">Drywall Completion Systems</a>
-//     </li>
-
-//     <li>
-//         <a href="#stc" class = "product-nav">Setting Type Compounds</a>
-//     </li>
-
-//     <li>
-//         <a href="#concrete" class = "product-nav">Concrete Finishing</a>
-//     </li>
-
-//     <li>
-//         <a href="#wallboard" class = "product-nav">Wallboard Tape</a>
-//     </li>
-// </ul>
-// </nav> 
